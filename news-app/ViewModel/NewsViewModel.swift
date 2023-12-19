537b442eeeaae5d7bddc87b615bbbff49ce353f6
Magistra <magistraagis@gmail.com>
2023-12-20 00:05:49 +0800

    switch to main thread

--- news-app/ViewModel/NewsViewModel.swift
+++ news-app/ViewModel/NewsViewModel.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
+
 class NewsViewModel: ObservableObject {
     @Published var news: [Post] = []
-    @Published var product: [Product] = []
+    @Published var HeadlineNews: [Post] = []
     
-    
+    @MainActor
     func getNewsList() async throws {
         guard let url = URL(string: "https://jakpost.vercel.app/api/category/business") else {
             throw URLError(.badURL)
@@ -29,6 +30,24 @@ class NewsViewModel: ObservableObject {
         
         let decodedData = try decoder.decode(NewsModel.self, from: data)
         self.news = decodedData.posts
-
+    }
+    
+    func getHeadlineNews() async throws {
+        guard let url = URL(string: "https://jakpost.vercel.app/api/category/business/tech") else {
+            throw URLError(.badURL)
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let decoder = JSONDecoder()
+        
+        let decodedData = try decoder.decode(NewsModel.self, from: data)
+        self.news = decodedData.posts
     }
 }
